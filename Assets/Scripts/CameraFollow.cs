d04610ec55b1d658dcfe38934f5e22364cc641f2
Koen Sanderse <koensanderse2000@gmail.com>
2018-03-12 15:58:51 +0100

    Full Unity push

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField]
+    GameObject FollowObject;
+    [SerializeField]
+    int H_Distance;
+    [SerializeField]
+    int V_Distance;
+
+    new Vector3 FO_Pos;
+    new Vector3 TargetPos;
+
+	void Start () {
+        //FO_Pos = FollowObject.transform.position;
+	}
+	
+	void Update () {
+        FO_Pos = FollowObject.transform.position;
+
+        TargetPos.y = FO_Pos.y + V_Distance;
+        TargetPos.x = FO_Pos.x;
+        TargetPos.z = FO_Pos.z - H_Distance;
+
+        this.transform.position = TargetPos;
+	}
+}
---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    [SerializeField]
+    GameObject MovementObject;
+    [SerializeField]
+    float speed;
+
+    Vector3 TempPos;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        TempPos = MovementObject.transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            TempPos.z += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            TempPos.x -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            TempPos.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            TempPos.z -= speed * Time.deltaTime;
+        }
+
+        MovementObject.transform.position = TempPos;
+    }
+}
